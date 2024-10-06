b84df6a0271573ce1f7d97260cd938f6fbef9d94
Dmdn <93030290+xiapeng01@users.noreply.github.com>
2024-10-06 17:17:31 +0800

    Update main.yml

--- BuildFaToolBox.bat
+++ BuildFaToolBox.bat
@@ -1,3 +1,2 @@
-dotnet ".\FaToolBox\Files\MyToolBox.exe" --version >>Version.txt
 iscc FaToolBox.iss /O".\FaToolBox\"  /D"SourceDirection=.\FaToolBox\Files\"  /D"LicenseFileName=.\FaToolBox\Files\LICENSE.txt" /D"MyAppVersion=1.2"
 
